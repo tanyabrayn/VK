024751433d65950f12dc1ac47e4df601e17a5821
Татьяна <123752579+tanyabrayn@users.noreply.github.com>
2023-06-27 22:39:28 +0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -6,27 +6,26 @@ from vk_api.utils import get_random_id
 from config import comunity_token, acces_token
 from core import VkTools
 
+
 class BotInterface():
 
-    def __init__(self,comunity_token, acces_token):
+    def __init__(self, comunity_token, acces_token):
         self.interface = vk_api.VkApi(token=comunity_token)
         self.api = VkTools(acces_token)
         self.params = None
 
-
     def message_send(self, user_id, message, attachment=None):
         self.interface.method('messages.send',
-                                {'user_id': user_id,
-                                'message': message,
-                                'attachment': attachment,
-                                'random_id': get_random_id()
-                                }
-                                )
-        
+                              {'user_id': user_id,
+                               'message': message,
+                               'attachment': attachment,
+                               'random_id': get_random_id()
+                               }
+                              )
+
     def event_handler(self):
         longpoll = VkLongPoll(self.interface)
 
-        
         for event in longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
                 command = event.text.lower()
@@ -37,9 +36,9 @@ class BotInterface():
                 elif command == 'поиск':
                     users = self.api.serch_users(self.params)
                     user = users.pop()
-                    #здесь логика дял проверки бд
-                    photos_user = self.api.get_photos(user['id'])                  
-                    
+                    # здесь логика дял проверки бд
+                    photos_user = self.api.get_photos(user['id'])
+
                     attachment = ''
                     for num, photo in enumerate(photos_user):
                         attachment += f'photo{photo["owner_id"]}_{photo["id"]}'
@@ -48,12 +47,17 @@ class BotInterface():
                     self.message_send(event.user_id,
                                       f'Встречайте {user["name"]}',
                                       attachment=attachment
-                                      ) 
-                    #здесь логика для добавленяи в бд
+                                      )
+                    # здесь логика для добавленяи в бд
                 elif command == 'пока':
                     self.message_send(event.user_id, 'пока')
                 else:
                     self.message_send(event.user_id, 'команда не опознана')
+
+
+if __name__ == '__main__':
+    bot = BotInterface(comunity_token, acces_token)
+    bot.event_handler()
 
 
 
