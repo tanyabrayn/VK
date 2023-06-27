4412c033ed667801a2f097a6a46698c1a23b85af
Татьяна <123752579+tanyabrayn@users.noreply.github.com>
2023-06-27 22:37:45 +0300

    Update data_store.py

--- data_store.py
+++ data_store.py
@@ -4,30 +4,34 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import Session
 
+from config import db_url_object
 
-# схема БД
 metadata = MetaData()
 Base = declarative_base()
+
+engine = create_engine(db_url_object)
 
 class Viewed(Base):
     __tablename__ = 'viewed'
     profile_id = sq.Column(sq.Integer, primary_key=True)
     worksheet_id = sq.Column(sq.Integer, primary_key=True)
 
+def add_user(engine, profile_id, worksheet_id):
+    with Session(engine) as session:
+        to_bd = Viewed(profile_id=profile_id, worksheet_id=worksheet_id)
+        session.add(to_bd)
+        session.commit()
 
-# добавление записи в бд
+def check_user(engine, profile_id, worksheet_id):
+    with Session(engine) as session:
+        from_bd = session.query(Viewed).filter(
+            Viewed.profile_id == profile_id,
+            Viewed.worksheet_id == worksheet_id
+        ).first()
+        return True if from_bd else False
 
-engine = create_engine(db_url_object)
-Base.metadata.create_all(engine)
-with Session(engine) as session:
-    to_bd = Viewed(profile_id=1, worksheet_id=1)
-    session.add(to_bd)
-    session.commit()
 
-# извлечение записей из БД
-
-engine = create_engine(db_url_object)
-with Session(engine) as session:
-    from_bd = session.query(Viewed).filter(Viewed.profile_id==1).all()
-    for item in from_bd:
-        print(item.worksheet_id)
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
+    res = check_user(engine, 2113, 1245121)
+    print(res)
