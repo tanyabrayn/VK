ac074ba4af3060a1294d2059c8d35bfa0438a0b3
Татьяна <123752579+tanyabrayn@users.noreply.github.com>
2023-06-28 20:54:27 +0300

    core.py

--- core.py
+++ core.py
@@ -1,35 +1,31 @@
 from pprint import pprint
-from datetime import datetime
 import vk_api
 from vk_api.exceptions import ApiError
 from config import acces_token
+from datetime import datetime
 
 class VkTools:
     def __init__(self, acces_token):
         self.vkapi = vk_api.VkApi(token=acces_token)
-
-    def _bdate_toyear(self, bdate):
-        user_year = bdate.split('.')[2]
-        now = datetime.now().year
-        return now - int(user_year)
 
     def get_profile_info(self, user_id):
 
         try:
             info, = self.vkapi.method('users.get',
                                       {'user_id': user_id,
-                                       'fields': 'city, sex, relation, bdate'
+                                       'fields': 'city,sex,relation,bdate'
                                        }
                                       )
-        except ApiError as error:
+        except ApiError as e:
             info = {}
-            print(f'error = {error}')
+            print(f'error = {e}')
 
         result = {'name': (info['first_name'] + ' ' + info['last_name']) if
                   'first_name' in info and 'last_name' in info else None,
                   'sex': info.get('sex'),
                   'city': info.get('city')['title'] if info.get('city') is not None else None,
-                  'year': self._bdate_toyear(info.get('bdate'))
+                  'year': datetime.now().year - int(info.get('bdate').split('.')[2])
+                  if info.get('bdate') is not None else None
                   }
         return result
 
@@ -46,11 +42,12 @@ class VkTools:
                                           'age_to': params['year'] + 3,
                                       }
                                       )
-        except ApiError as error:
+        except ApiError as e:
             users = []
-            print(f'error = {error}')
+            print('Вышла ошибочка ...')
+            print(f'error = {e}')
 
-        result = [{'name': item['first_name'] + " " + item['last_name'],
+        result = [{'name': item['first_name'] + ' ' + item['last_name'],
                    'id': item['id']
                    } for item in users['items'] if item['is_closed'] is False
                   ]
@@ -58,34 +55,31 @@ class VkTools:
         return result
 
     def get_photos(self, id):
-        photos = self.vkapi.method('photos.get',
-                                 {'user_id': id,
-                                  'album_id': 'profile',
-                                  'extended': 1
-                                  }
-                                 )
         try:
-            photos = photos['items']
-        except KeyError:
-            return []
+            photos = self.vkapi.method('photos.get',
+                                       {'owner_id': id,
+                                        'album_id': 'profile',
+                                        'extended': 1
+                                        }
+                                       )
+        except ApiError as e:
+            photos = {}
+            print(f'error = {e}')
 
-        res = []
+        result = [{'owner_id': item['owner_id'],
+                   'id': item['id'],
+                   'likes': item['likes']['count'],
+                   'comments': item['comments']['count']
+                   } for item in photos['items']
+                  ]
+        '''сортировка по лайкам и комментариям, выводим 3 фото'''
+        result.sort(key=lambda x: (x['likes'], x['comments']), reverse=True)
 
-        for photo in photos:
-            res.append({'owner_id': photo['owner_id'],
-                        'id': photo['id'],
-                        'likes': photo['likes']['count'],
-                        'comments': photo['comments']['count'],
-                        }
-                       )
-
-        res.sort(key=lambda x: x['likes'] + x['comments'] * 10, reverse=True)
-
-        return res
+        return result[:3]
 
 
 if __name__ == '__main__':
-    user_id =()
+    user_id = ()
     tools = VkTools(acces_token)
     params = tools.get_profile_info(user_id)
     worksheets = tools.search_worksheet(params, 20)
